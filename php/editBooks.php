8d092f43f4109a7ac86d63b73282b27dd8f2ee0b
Sheldon Lee <sheldon.m.lee@gmail.com>
2020-12-29 18:13:40 +0000

    Moved handling of edit book list to editBooks.php

---
+++ php/editBooks.php
@@ -0,0 +1,27 @@
+<?php
+require_once("login.php");
+
+function getEditBooksList()
+{
+	require("details.php");
+	if (!loggedIn()) return;
+	$dbConn = getConnection($details);
+
+	$results = $dbConn->query("SELECT bookISBN, bookTitle FROM NBL_books");
+
+	$output = "<div id=\"editBooks\">\n";
+	$output .= "<li>\n";
+	foreach($results as $row) {
+		$output .= "<li>
+			<button onclick=\"getBody('pages/editBookForm.php?bookISBN={$row["bookISBN"]}')\">Edit</button>
+			{$row["bookTitle"]}
+			</li>\n";
+	}
+	$output .= "</li>\n";
+	$output .= "</div>\n";
+
+	$dbConn = null;
+	$results = null;
+	return $output;
+}
+?>
