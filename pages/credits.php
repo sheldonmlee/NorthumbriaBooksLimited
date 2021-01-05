b738777a3fe55cfe6b6164f89db7565df7f85377
Sheldon Lee <sheldon.m.lee@gmail.com>
2021-01-05 19:01:16 +0000

    Addet credits.

---
+++ pages/credits.php
@@ -0,0 +1,28 @@
+<h1>Credits:</h2>
+<p>A list of sources that aided the production of this website.</p>
+<section id="credits_js">
+	<h2>Javascript sources:</h2>
+		<ul>
+			<li><a href="https://www.w3schools.com/jsref/met_document_queryselector.asp">Query selector js</a></li>
+			<li><a href="https://www.w3schools.com/js/js_htmldom_css.asp">Changing CSS styles in js</a></li>
+			<li><a href="https://www.w3schools.com/jsref/prop_checkbox_checked.asp">Input Checkbox state js</a></li>
+			<li><a href="https://www.w3schools.com/jsref/prop_submit_disabled.asp">Disabled state for submit js</a></li>
+			<li><a href="https://www.w3schools.com/jsref/prop_submit_disabled.asp">Constraint validation</a></li>
+		</ul>
+</section>
+
+<section id="credits_css">
+	<h2>CSS resources</h2>
+		<ul>
+			<li><a href="https://www.w3schools.com/php/func_var_is_numeric.asp">CSS overflow property</a></li>
+			<li><a href="https://www.w3schools.com/php/func_var_is_numeric.asp">Styling links</a></li>
+		</ul>
+</section>
+
+<section id="credits_php">
+	<h2>PHP sources:</h2>
+		<ul>
+			<li><a href="https://www.w3schools.com/php/func_var_is_numeric.asp">Checking if string is numeric</a></li>
+			<li><a href="https://www.w3schools.com/php/func_var_is_numeric.asp">PHP class related things</a></li>
+		</ul>
+</section>
