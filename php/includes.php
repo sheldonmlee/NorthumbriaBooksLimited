1c1a2c00c3e101b5066b95c71a628a1e45231446
Sheldon Lee <sheldon.m.lee@gmail.com>
2021-01-05 19:52:59 +0000

    Added comments.

--- php/includes.php
+++ php/includes.php
@@ -1,4 +1,6 @@
 <?php
 if (!defined("ROOT")) define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/NorthumbriaBooksLimited");
+// This is a collection of oftenly used functions that I have written for workshops, 
+// or for the assignment that I could reuse.
 require_once(ROOT."/lib/phpUtils/utils.php");
 ?>
